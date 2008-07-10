66643007f50d511667bfa79f7dbe3bfe48ed876c
Wes Oldenbeuving <narnach@gmail.com>
2008-07-10 18:22:53 +0200

    Added a simple test for each method on Md5Wrapper.

--- test/md5_wrapper_test.rb
+++ test/md5_wrapper_test.rb
@@ -1,8 +1,17 @@
 require 'test/unit'
+require 'md5_wrapper'
 
 class Md5WrapperTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def setup
+    @fixture = File.expand_path(File.join(File.dirname(__FILE__),'fixtures','test.txt'))
+    @md5sum = 'd8e8fca2dc0f896fd7cb4cb0031ba249'
+  end
+  
+  def test_get_md5sum
+    assert_equal @md5sum, Md5Wrapper.get_md5sum(@fixture)
+  end
+  
+  def test_check_md5sum
+    assert Md5Wrapper.check_md5sum(@fixture, @md5sum)
   end
 end
