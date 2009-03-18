a558a02af7d17cdc5b3c6733dccf14d644f38d4a
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2009-03-18 17:15:39 +0100

    Made Md5Wrapper work with filenames with spaces.

--- lib/md5_wrapper.rb
+++ lib/md5_wrapper.rb
@@ -5,23 +5,23 @@ class Md5Wrapper
     def check_md5sum(filename, md5sum)
       raise ArgumentError, "Invalid md5sum" unless md5sum.size == 32
       raise ArgumentError, "Invalid file" unless File.exist? filename and File.file? filename and File.readable? filename
-      
+
       if RUBY_PLATFORM =~ /darwin/
-        md5 = `md5 #{filename} 2>&1`.strip[-32,32]
+        md5 = `md5 "#{filename}" 2>&1`.strip[-32,32]
         return md5 == md5sum
       else # linux
         cmd = "echo \"#{md5sum} *#{filename}\" | md5sum -c --status 2>&1"
         return system(cmd)
       end
     end
-    
+
     # Extract the md5 hexdigest from a file.
     def get_md5sum(filename)
       raise ArgumentError, "Invalid file" unless File.exist? filename and File.file? filename and File.readable? filename
       if RUBY_PLATFORM =~ /darwin/
-        return `md5 #{filename} 2>&1`.strip[-32,32]
+        return `md5 "#{filename}" 2>&1`.strip[-32,32]
       else # linux
-        return `md5sum -b #{filename} 2>&1`[0,32]
+        return `md5sum -b "#{filename}" 2>&1`[0,32]
       end
     end
   end
--- test/md5_wrapper_test.rb
+++ test/md5_wrapper_test.rb
@@ -1,17 +1,38 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'test/unit'
 require 'md5_wrapper'
+require 'tempfile'
 
 class Md5WrapperTest < Test::Unit::TestCase
   def setup
     @fixture = File.expand_path(File.join(File.dirname(__FILE__),'fixtures','test.txt'))
     @md5sum = 'd8e8fca2dc0f896fd7cb4cb0031ba249'
   end
-  
+
   def test_get_md5sum
     assert_equal @md5sum, Md5Wrapper.get_md5sum(@fixture)
   end
-  
+
   def test_check_md5sum
     assert Md5Wrapper.check_md5sum(@fixture, @md5sum)
+  end
+
+  def test_get_md5sum_for_filenames_with_spaces
+    Tempfile.open('text file with spaces.txt') do |f|
+      File.open(f.path,'w') do |fp|
+        fp.puts 'test'
+      end
+      assert_equal @md5sum, Md5Wrapper.get_md5sum(f.path)
+    end
+  end
+
+  def test_check_md5sum_for_filenames_with_spaces
+    Tempfile.open('text file with spaces.txt') do |f|
+      File.open(f.path,'w') do |fp|
+        fp.puts 'test'
+      end
+      assert Md5Wrapper.check_md5sum(f.path,@md5sum)
+    end
   end
 end
